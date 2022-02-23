b407bbcb2f43b96c514d495c8fdf8c52e60303f3
EC2 Default User <ec2-user@ip-172-31-89-42.ec2.internal>
2022-02-23 13:00:23 +0000

    [add]Rails6-bookers2-debug-main

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,19 @@
+class PostCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    comment = current_user.post_comment.new(post_comment_params)
+    comment.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+   PostComment.find(params[:id]).destroy
+   redirect_to request.referer
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+end
